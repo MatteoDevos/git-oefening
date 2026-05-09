0b19381c0bb43bc047fb346c1cbdb586211505c3
Matteo Devos <matteo.devos@telenet.be>
2026-05-09 17:29:11 +0200

    Added avarage calculation

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ students = {
     "Noah": 12
 }
 
+total = 0
+
 for name, score in students.items():
     print(f"{name}: {score}/20")
+    total += score
+
+average = total / len(students)
+
+print(f"\nGemiddelde score: {average:.2f}/20")
